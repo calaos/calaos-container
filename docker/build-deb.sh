7e8aaa7d9ec7939f46010cd76289d391d0ce1fa6
Raoul Hecky <raoul.hecky@gmail.com>
2023-07-14 16:53:01 +0200

    Add version to package

--- docker/build-deb.sh
+++ docker/build-deb.sh
@@ -3,9 +3,17 @@
 set -e
 
 workdir="/work"
-
+version="$1"
 
 cd $workdir || exit
+
+if [ -z "$version" ]
+then
+    version=$(git describe --long --tags --always)
+fi
+
+sed -i "s/VERSION = 1.0.0/VERSION = $version/g" debian/rules
+sed -i "s/1.0-0/$version/g" debian/changelog
 
 apt-get build-dep -y .
 dpkg-buildpackage -us -uc -b
