8afbc0bd4ef28caed8c3e22f5abaf4a903e80c1a
Raoul Hecky <raoul.hecky@gmail.com>
2023-07-16 14:51:04 +0200

    more work

--- docker/build-deb.sh
+++ docker/build-deb.sh
@@ -12,6 +12,8 @@ then
     version=$(git describe --long --tags --always)
 fi
 
+git config --global --add safe.directory $workdir
+
 sed -i "s/VERSION = 1.0.0/VERSION = $version/g" debian/rules
 sed -i "s/1.0-0/$version/g" debian/changelog
 
