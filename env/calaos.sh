21512a4981b2a5d351cbfbb469eb1911aa86715a
Raoul Hecky <raoul.hecky@gmail.com>
2023-07-19 16:32:48 +0200

    add env to profile

---
+++ env/calaos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This file is installed in /etc/profile.d and will allow shell users to use calaos tools directly
+
+# calaos_ddns
+export CALAOS_HAPROXY_PATH="/mnt/calaos/haproxy"
+export CALAOS_CERT_FILE="/mnt/calaos/ssl/haproxy"
+export CALAOS_CONFIG="/mnt/calaos/config"
+export CALAOS_HAPROXY_TEMPLATE_PATH="/usr/share/calaos-ddns"
+
+alias calaos_config='podman exec calaos-server /opt/bin/calaos_config'
+alias calaos_1wire='podman exec calaos-server /opt/bin/calaos_1wire'
+alias wago_test='podman exec calaos-server /opt/bin/wago_test'
+alias calaos_mail='podman exec calaos-server /opt/bin/calaos_mail'
