86060296160851829af04b8661e05ba6eb2a1b22
Raoul Hecky <raoul.hecky@gmail.com>
2023-07-19 08:56:37 +0200

    add libpod/go-systemd deps

--- main.go
+++ main.go
@@ -15,31 +15,7 @@ import (
 	"github.com/fatih/color"
 	cli "github.com/jawher/mow.cli"
 	"github.com/sirupsen/logrus"
-	//	"github.com/containers/podman/v4/pkg/bindings"
-	//	"github.com/containers/podman/v4/pkg/bindings/images"
-	//
-	// "github.com/coreos/go-systemd/v22/dbus"
-	// godbus "github.com/godbus/dbus/v5"
 )
-
-/*
- Podman API Docs:
- https://pkg.go.dev/github.com/containers/podman/v4@v4.5.1/pkg/bindings#section-readme
-*/
-/*
-func main() {
-	conn, err := bindings.NewConnection(context.Background(), "unix://run/podman/podman.sock")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	_, err = images.Pull(conn, "ghcr.io/calaos/calaos_home", nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-*/
 
 const (
 	DefaultConfigFilename = "/etc/calaos-container.toml"
--- models/models.go
+++ models/models.go
@@ -1,9 +1,17 @@
 package models
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	logger "github.com/calaos/calaos-container/log"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/containers/podman/v4/pkg/bindings"
+	"github.com/containers/podman/v4/pkg/bindings/images"
+	"github.com/coreos/go-systemd/v22/dbus"
 )
 
 var (
@@ -23,4 +31,33 @@ func Init() (err error) {
 // Shutdown models
 func Shutdown() {
 
+}
+
+/*
+ Podman API Docs:
+ https://pkg.go.dev/github.com/containers/podman/v4@v4.5.1/pkg/bindings#section-readme
+*/
+
+func Pull(image string) (err error) {
+	conn, err := bindings.NewConnection(context.Background(), "unix://run/podman/podman.sock")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = images.Pull(conn, "ghcr.io/calaos/calaos_home", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
+func StopUnit(unit string) (err error) {
+	conn, err := dbus.NewWithContext(context.Background())
+	defer conn.Close()
+
+	_, err = conn.StopUnitContext(context.Background(), unit, "replace", nil)
+
+	return err
 }
