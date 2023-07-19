d22ca10293a00a6691e07e53688f69751150d918
Raoul Hecky <raoul.hecky@gmail.com>
2023-07-19 15:03:40 +0200

    Improve deb build script to detect pkg name

--- docker/build-deb.sh
+++ docker/build-deb.sh
@@ -17,8 +17,10 @@ fi
 sed -i "s/VERSION = 1.0.0/VERSION = $version/g" debian/rules
 sed -i "s/1.0-0/$version/g" debian/changelog
 
+pkg_name=$(grep "Package: " debian/control | sed 's/Package: //')
+
 apt-get build-dep -y .
 dpkg-buildpackage -us -uc -b
 
 mkdir $workdir/pkg
-mv ../calaos-container_*.deb $workdir/pkg
+mv ../"{$pkg_name}"_*.deb $workdir/pkg
