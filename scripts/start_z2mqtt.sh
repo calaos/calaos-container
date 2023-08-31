02bf5b0a3c15fdebcd7daf24a330469d8eb614cc
Raoul Hecky <raoul.hecky@gmail.com>
2023-08-31 14:00:10 +0200

    Map device from config file into zigbee2mqtt ct

---
+++ scripts/start_z2mqtt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+cidfile=$1
+[ -z "$cidfile" ] && {
+    echo "Missing arg"
+    exit 1
+}
+
+devlist()
+{
+    grep '/dev/' configuration.yaml | awk '{print $2}'
+}
+
+append_dev_opts()
+{
+    local device=''
+
+    while read -r device ; do
+        echo " --device $device "
+    done < <(devlist)
+}
+
+# shellcheck disable=SC2046
+
+podman run \
+        --name=zigbee2mqtt \
+        --cidfile="${cidfile}" --replace --rm --tz=local \
+        --sdnotify=conmon -d \
+        -v /mnt/calaos/zigbee2mqtt:/app/data \
+        --network=host \
+        $(append_dev_opts) \
+        docker.io/koenkk/zigbee2mqtt
